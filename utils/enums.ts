003c87a4d4c65ee288ee6cb1c08bcbe540923448
konst <my@email.com>
2025-06-16 21:23:51 +0300

    Add initial data model

---
+++ utils/enums.ts
@@ -0,0 +1,17 @@
+export enum Roles {
+  USER,
+  ADMIN,
+}
+
+export enum MatchType {
+  BEST_OF_1,
+  BEST_OF_3,
+  BEST_OF_5,
+}
+
+export enum StatusOfEvent {
+  WILL_START,
+  ACTIVE,
+  ENDED,
+  ARCHIVED,
+}
